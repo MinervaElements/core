4a10e3791f24b61accb0ce3511a1af7cb20055fd
Sbleit <zptrack24@gmail.com>
2023-07-22 17:20:45 -0400

    añadi más tipos

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,17 @@ export interface Iroutes{
   name:string;
   link:string;
 }
+interface Ifeatures{
+  name:string;
+  status:boolean
+}
+interface Ipricing{
+  name:string;
+  price:number;
+  time:string;
+}
+export interface IplansData{
+  color:string;
+  pricing:Ipricing;
+  features:Ifeatures[];
+}
