2868598701444081d3ab56d01ae63fe2c69fc891
Eli-ezer ramirez <ramirez.ruiz.eliezer.reuven@gmail.com>
2023-07-28 00:17:26 -0400

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Header from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
+
 const inter = Poppins({ subsets: ["latin"], weight: ["400","500","600","700","800"] });
 
 export const metadata: Metadata = {
