d6617dcbb00252a3d4f567b4b4fb971f690ff605
Eli-ezer ramirez <ramirez.ruiz.eliezer.reuven@gmail.com>
2023-07-30 23:54:41 -0400

    update Faq

--- src/app/components/Faq/Faq.tsx
+++ src/app/components/Faq/Faq.tsx
@@ -3,7 +3,29 @@ import styles from "./Faq.module.css";
 export const Faq = () => {
   return (
     <>
-    <h1>FAQ</h1>
+    <h1>Frequently Asked Questions</h1>
+
+    <div className="">
+        <ul>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+        </ul>
+    </div>
+    <div className="">
+        <ul>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+        </ul>
+    </div>
+
     </>
   );
 };
