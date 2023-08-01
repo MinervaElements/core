930fcd63058f5a2a2ee3affb4f8cacb0382d5222
Eli-ezer ramirez <ramirez.ruiz.eliezer.reuven@gmail.com>
2023-07-31 21:34:09 -0400

    update Faq

--- src/app/components/Faq/Faq.tsx
+++ src/app/components/Faq/Faq.tsx
@@ -1,31 +1,71 @@
 import styles from "./Faq.module.css";
 
-export const Faq = () => {
+const Faq = () => {
   return (
-    <>
-    <h1>Frequently Asked Questions</h1>
-
-    <div className="">
-        <ul>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-        </ul>
+    <div>
+      <h2>FAQ - Preguntas Frecuentes</h2>
+      <div>
+        <h3>1. ¿Qué tipo de servicios de páginas web ofrece tu ecommerce?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>2. ¿Cómo puedo empezar a trabajar contigo en mi página web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>3. ¿Cuál es el proceso de diseño y desarrollo de una página web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>4. ¿Cuánto tiempo tomará crear mi sitio web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>5. ¿Puedo tener un diseño personalizado para mi página web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>6. ¿Cuál es el costo aproximado de un sitio web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>7. ¿Qué información necesitas de mí para comenzar el desarrollo de mi página web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>8. ¿Ofreces servicios de mantenimiento y actualización de sitios web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>9. ¿Cuál es tu política de pagos y formas de pago aceptadas?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>10. ¿Qué sucede si no estoy satisfecho con el diseño de mi sitio web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>11. ¿Ofreces servicios de alojamiento web?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>12. ¿Puedo solicitar funciones específicas para mi página web, como un formulario de contacto o integración con redes sociales?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>13. ¿Proporcionas servicios de optimización para motores de búsqueda (SEO)?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>14. ¿Cómo puedo ponerme en contacto contigo si tengo preguntas o inquietudes adicionales?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
+      <div>
+        <h3>15. ¿Tienes ejemplos de trabajos anteriores que hayas realizado?</h3>
+        <p>Tu respuesta aquí...</p>
+      </div>
     </div>
-    <div className="">
-        <ul>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-            <li> </li>
-        </ul>
-    </div>
-
-    </>
   );
 };
+
+export default Faq;
